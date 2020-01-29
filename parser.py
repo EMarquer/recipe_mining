3478bcd05fbd642e2a05e381f1c4e568103ce067
Maxime GUILLAUME <maxime.guillaume2@etu.univ-lorraine.fr>
2020-01-29 15:30:01 +0100

    Add parser

--- parser.py
+++ parser.py
@@ -14,8 +14,48 @@ for recipe in recipe_files:
 
     current_recipe = dict()
 
-    current_recipe['ingredients'] = xml_file.getElementsByTagName("IN")
-    current_recipe['tools'] = xml_file.getElementsByTagName("US")
-    
-    current_recipe['content'] = xml_file.getElementsByTagName("PR")[0]
+    current_recipe['ingredients'] = [e.firstChild.nodeValue for e in xml_file.getElementsByTagName("IN")]
+    current_recipe['tools'] = [e.firstChild.nodeValue for e in xml_file.getElementsByTagName("US")]
+    current_recipe['content'] = xml_file.getElementsByTagName("PR")[0].firstChild.nodeValue
+    recipes.append(current_recipe)
 
+# Set of ingredients
+ingredients = []
+for r in recipes:
+    ingredients.extend(r['ingredients'])
+ingredients = set(ingredients)
+print("Ingredients size :",len(ingredients))
+
+# set of tools
+tools = []
+for r in recipes:
+    tools.extend(r['tools'])
+tools = set(tools)
+print("Tools size : ",len(tools))
+
+
+def get_prep(recipe):
+    """Returns the content"""
+    return recipe['content']
+
+def generate_table(recipe):
+    """Returns 2 dicts """
+
+    
+    tools_in_recipe = set(recipe['tools']) 
+    ingredient_in_recipe = set(recipe['ingredients'])
+
+    
+    tool_matrix =dict()
+    ingredient_matrix = dict()
+    
+    for t in tools:
+     tool_matrix[t] = t in tools_in_recipe
+
+    for i in ingredients:
+        ingredient_matrix[i] = i in ingredient_in_recipe
+     
+    return tool_matrix, ingredient_matrix
+    
+
+print(generate_table(recipes[0]))
